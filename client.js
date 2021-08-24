eab4679bd566e7b8134247116b934675b7c4c3e3
Russell Barr <rbarrsquared@gmail.com>
2021-08-24 16:31:34 -0500

    bonus calculator, needs final touches

--- client.js
+++ client.js
@@ -32,6 +32,55 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+//function to take in employee obj
+//return new obj with bonusPercentage, totalCompensation, totalBonus
+
+function processEmpBonus (obj) {
+  let processedBonuses = {};
+function bonusPercentCal(empObj) {
+  let employeeSal = parseInt(empObj.annualSalary);
+
+  let bonusPercentage = 0;
+
+  //star review
+  for (let employee of empObj) {
+    if (employee.reviewRating <= 2) {
+      //no bonus
+    } else if (employee.reviewRating === 3) {
+      bonusPercentage = 0.04;
+    } else if (employee.reviewRating === 4) {
+      bonusPercentage = 0.06;
+    } else if (employee.reviewRating === 5) {
+      bonusPercentage = 0.1;
+    }
+  }
+  //loop for employee number, 4 digits for 5%
+  for (let emp of empObj) {
+    if (emp.employeeNumber.length === 4) {
+      bonusPercentage = bonusPercentage + (employeeSal * 0.05)
+    }
+    //loop for the employee salary, if (salary > $65k) - 1% from salary
+    for (let input of empObj) {
+      if (empObj.employeeSal > 65000) {
+        bonusPercentage = bonusPercentage - 0.01;
+      }
+    }
+    // bonus percentage set to 13% max, 0% as bottom.
+    if (bonusPercentage > 0.13) {
+      bonusPercentage = 0.13;
+    } else if (bonusPercentage < 0) {
+      bonusPercentage = 0;
+    }
+  }
+  return bonusPercentage;
+} // end of bonusPercentage
+let totalBonus = bonusPercentCal * employeeSal;
+let totalCompensation = totalBonus + employeeSal;
+
+
+
+
+}// end of processEmpBonus
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
@@ -40,4 +89,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(bonusCalculator(employees));
